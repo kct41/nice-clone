4e9f229d42161d4c67108ea936e059747f0f1cec
SOMI <som2mos@naver.com>
2024-07-12 16:57:03 +0900

    0712 최종

--- src/common/Notice.js
+++ src/common/Notice.js
@@ -13,7 +13,7 @@ function Notice() {
                 </a>
                 <div className="list">
                     <div className="title">
-                        <p>시스템 정기점검으로 인한 서비스 이용제한 웅앵웅앵aaaa</p>
+                        <p>시스템 정기점검으로 인한 서비스 이용제한</p>
                     </div>
                     <div className="date">
                         <p>2023.03.27</p>
